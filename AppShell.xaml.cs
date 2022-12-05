50527f894aff67f36c76347cb3a92a8b61779ec0
SokoFromNZ <soko17@soko.cc>
2022-12-05 11:41:04 +0100

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiNoLabelWrappingInGrid;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiNoLabelWrappingInGrid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
